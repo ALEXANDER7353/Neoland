ecf87d4e1414a3a5b24803f365e4ec30f303ccc1
´´ALEXANDR7353´´ <zapataalexander693@gmail.com>
2024-10-29 22:13:16 +0100

    dom

--- ejercicios/DOM/app.js
+++ ejercicios/DOM/app.js
@@ -12,6 +12,25 @@ console.log(h);
 
 //1.3 Usa querySelector para mostrar por consola todos los p
 
+// Selecciona todos los elementos <p> en el documento y los almacena en la variable 'parrafos'
 const parrafos = document.querySelectorAll('p');
+
+// Recorre cada elemento en 'parrafos' usando forEach y lo muestra en la consola
 parrafos.forEach(parrafo => console.log(parrafo));
 
+
+// 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+
+document.querySelectorAll('.pokemon').forEach(element => console.log(element));
+
+//1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
+//data-function="testMe".
+
+document.querySelectorAll('[data-function="testMe"]').forEach(Element => console.log(Element));
+
+
+//1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
+//data-function="testMe".
+
+const thirdCharacter = document.querySelectorAll('[data-function="testMe"]')[2];
+console.log(thirdCharacter);
