a9c50652dec4494b2623a65ac339d560f9570edf
´´ALEXANDR7353´´ <zapataalexander693@gmail.com>
2024-12-09 20:41:29 +0100

    Guardando los cambios en feature-alexander

---
+++ server-mongo/config/db.js
@@ -0,0 +1,17 @@
+
+
+
+const mongoose = require('mongoose')
+
+const connectDB= async () =>{
+    try {
+        mongoose.connect(process.env.MONGO_URI,{
+            
+        })
+    } catch (error) {
+        console.log("🚀 ~ connectDB ~ error:", error)
+        
+    }
+}
+
+module.exports= connectDB
