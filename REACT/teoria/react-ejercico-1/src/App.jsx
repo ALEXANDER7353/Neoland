92263bd75f9a26d4ae446c4f44c3200e1276922f
´´ALEXANDR7353´´ <zapataalexander693@gmail.com>
2025-01-15 22:04:32 +0100

    react-teoria-ejemplos

---
+++ REACT/teoria/react-ejercico-1/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+
+
+function App() {
+ const [time, setTime] = useState(new Date ())
+
+useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000)
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <>
+    <h1>hora actual :{time.toLocaleTimeString()}</h1>
+    </>
+
+  )
+}
+
+export default App
